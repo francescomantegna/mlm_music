8765afd110c81858d13158117baefc9bd651e2db
Phillip Alday <phillip.alday@mpi.nl>
2020-01-09 18:42:48 +0100

    model optimization

--- mlmmusic.R
+++ mlmmusic.R
@@ -4,7 +4,8 @@ library("tidyverse")  # for plotting and data manipulation
 library("lme4")
 library("car")
 library("effects")
-library("caret")
+# library("caret") # for multicollinarity check
+library("MASS") # for contr.sdif
 library("lattice")
 library("R.matlab")
 library("eegUtils") # from https://github.com/craddm/eegUtils/
@@ -13,21 +14,44 @@ options(contrasts = c("contr.Sum","contr.Poly"))
 
 #' For the voltage, we have 59 preprocessed channels:
 #' 64 electrodes - 4 oculograms - 1 mastoid
-dat <- read_csv("mlm_inputmusic.csv")
+dat <- read_csv(here("mlm_inputmusic.csv"))
 
-X <- model.matrix(~ BS + condition * x * y * z,data=dat)
-lcs <- findLinearCombos(X)
-colnames(X)[lcs$remove]
+dat$condition <- factor(dat$condition)
+
+
+# this sets the contrasts for condition as aug4 < dominant < tonic
+contrasts(dat$condition) <- contr.sdif(c("aug4","dominant","tonic"))
+
+# we omit the multicollinearity check because we have an orthogonal design
+# X <- model.matrix(~ BS + condition * x * y * z,data=dat)
+# lcs <- findLinearCombos(X)
+# colnames(X)[lcs$remove]
 
 # so that I don't have to remember the different options different optimizers have...
 bobyqa <- lmerControl(optimizer="bobyqa",optCtrl=list(maxfun=1e6))
 nlopt <- lmerControl(optimizer="nloptwrap",optCtrl=list(maxeval=1e6))
 
-system.time(m <- lmer(scale(N5) ~ 1 + scale(BS) + condition * group * scale(x) * scale(y) * scale(z) +
+# these were extracted from a previous model fit. they will speed up future fits if they're right,
+# but not prevent convergence to the correct values if they're wrong
+# they will speed up
+
+#+ theta
+theta <- c(0.18648574040049,-0.0443144981555326,0.0307819282071667,
+           0.161675512694921,-0.0355269822704287,0.180234962926686,
+           0.0625669832864284,-0.00162793523354175,0.0225908831188544,
+           0.103424412839264,-0.035597248934163,0.117197823819823)
+
+# this gives a warning about scaling; however, the model still converges on the
+# original scale, so I would leave it. If you add scaling in, then I would only
+# do it for N5 and BS and not the for scalp topography
+
+#+ condition_model, cache=TRUE, depends="theta"
+system.time(m <- lmer(N5 ~ 1 + BS + condition * group * x * y * z +
                         (1 + condition | subject_id) +
                         (1 + condition | scale_id),
                       data=dat,
-                      control=bobyqa,
+                      control=nlopt,
+                      start=theta,
                       REML=FALSE))
 
 print(summary(m),correlation=FALSE,symbolic.cor=TRUE)
